f8313a2e527ddf76e9ee0443fd1f5ea443712f58
Andrei Svyatilik <helpmantemryk@gmail.com>
2022-02-27 04:24:53 +0300

    FINAL_TASK complete спасибо за помощь

--- task_final/solution.js
+++ task_final/solution.js
@@ -53,7 +53,7 @@ function sendRequest(name, phone, address, goods, sum) {
     data.order.sum = sum;
 	 data.client = name + ' ' + phone;
 
-        let jsonData = JSON.stringify(data);
+        let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
